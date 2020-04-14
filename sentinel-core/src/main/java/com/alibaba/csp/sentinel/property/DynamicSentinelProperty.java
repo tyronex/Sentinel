d10cfc3e5a195ebd858f4514c7276c4bdf23d544
huangtao <huangtao@xsyxsc.com>
2020-04-14 17:47:52 +0800

    add sentinel properties

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/property/DynamicSentinelProperty.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/property/DynamicSentinelProperty.java
@@ -15,11 +15,11 @@
  */
 package com.alibaba.csp.sentinel.property;
 
+import com.alibaba.csp.sentinel.log.RecordLog;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.alibaba.csp.sentinel.log.RecordLog;
 
 public class DynamicSentinelProperty<T> implements SentinelProperty<T> {
 
@@ -52,7 +52,7 @@ public class DynamicSentinelProperty<T> implements SentinelProperty<T> {
         }
         RecordLog.info("[DynamicSentinelProperty] Config will be updated to: " + newValue);
 
-        value = newValue;
+       value = newValue;
         for (PropertyListener<T> listener : listeners) {
             listener.configUpdate(newValue);
         }
